5f3c4643c590275fdadb388de5d7fe1d01ad975b
Sasha <45601549+Bendersnatch@users.noreply.github.com>
2025-01-16 19:28:00 +0100

    Update Main.java

--- Battle Arena/battlearena/battlearena/Main.java
+++ Battle Arena/battlearena/battlearena/Main.java
@@ -3,11 +3,14 @@ package battlearena;
 
 public class Main {
     public static void main(String[] args) {
-        Enemy zombie = new Zombie( 10, 1);
+        Zombie zombie = new Zombie( 10, 1);
+        Ogre ogre = new Ogre(20, 3);
 
+        System.out.println("Nemico ha " + zombie.getHealthPoints() + " punti di vita e ha " + zombie.getAttackDamage() + " punti d'attacco.");
 
-       System.out.println("Nemico ha " + zombie.getHealthPoints() + " punti di vita e ha " + zombie.getAttackDamage() + " punti d'attacco.");
+        zombie.talk();
+        zombie.spreadDisease();
 
-
+        ogre.talk();
     }
 }
