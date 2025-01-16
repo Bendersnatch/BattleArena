1fb783aaee04ed2cfad6f6ddf4b0fb59307661f1
Sasha <45601549+Bendersnatch@users.noreply.github.com>
2025-01-16 19:32:08 +0100

    Update Zombie.java
    add override and metods to zombie class

--- Battle Arena/battlearena/battlearena/Zombie.java
+++ Battle Arena/battlearena/battlearena/Zombie.java
@@ -1,7 +1,17 @@
 package battlearena;
 
 public class Zombie extends Enemy{
-    public Zombie( int healthPoints, int attackDamage) {
+    public Zombie( int healthPoints, int attackDamage)
+    {
         super(healthPoints, attackDamage);
+    }
+
+    @Override
+    public void talk() {
+        System.out.println("Grrrrmlll...");
+    }
+
+    public void spreadDisease(){
+        System.out.println("Zombie cerca di contaggiarti con il coviddi!");
     }
 }
