ad1ab896e012f9d1ff407265d5285c67a92b7768
Sasha <45601549+Bendersnatch@users.noreply.github.com>
2025-01-19 20:59:21 +0100

    Update Ogre.java
    add special attack random counter

--- Battle Arena/battlearena/battlearena/Ogre.java
+++ Battle Arena/battlearena/battlearena/Ogre.java
@@ -9,4 +9,13 @@ public class Ogre extends Enemy{
     public void talk(){
         System.out.println("WAAAAAGHH!, urla l'ogro sputtando la saliva...");
     }
+
+    @Override
+    public void specialAttack(){
+        boolean didSpecialAttackWork = Math.random() < .20;
+        if(didSpecialAttackWork){
+            setAttackDamage(getAttackDamage() + 4);
+            System.out.println("Ogre va in ira e va in carica!");
+        }
+    }
 }
